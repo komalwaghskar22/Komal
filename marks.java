695421b97ec1ef521834eb796df5dfbec5c388f4
Yashrajbhosale <bhosaleyashraj931@gmail.com>
2026-07-30 16:38:02 +0530

    Successfully commited

---
+++ marks.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class marks
+{
+	public static void main(String args[])
+	{
+		int score=80;
+		if(score>=80)
+			System.out.println("Grade A");
+		else
+			System.out.println("Grade B");
+	}
+}
